ea3ab9909d13d8cfeb80b868c32a242bc2838c8b
Sean B <butts.sean01@gmail.com>
2020-08-03 08:02:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 #def reverse_each_word(sentence1)
-  sentence1.split.collect {|w| w.reverse}.join(" ")
-end
+#  sentence1.split.collect {|w| w.reverse}.join(" ")
+#end
 
 def reverse_each_word(sentence1)
   sentence1.split
